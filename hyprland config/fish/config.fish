1314e9e54429748c94e9c01edc5b07139588d952
KrvyFT <KrvyFT@proton.me>
2024-07-18 22:14:02 +0800

    Up

---
+++ hyprland config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias img 'kitty +kitten icat'
+alias diff 'kitty +kitten diff'
+alias vim 'nvim'
+alias ex 'extract'
+alias aria 'aria2c -s 4 -x 8 -j 16 -c'
